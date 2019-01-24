705c3c5d853c815b76f958ed7d862ae465dda26d
Artem Los <arteml@kth.se>
2019-01-24 15:21:56 +0100

    Finish

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'cryptolens-licensing',         # How you named your package folder (MyLib)
-  packages = ['cryptolens-licensing'],   # Chose the same as "name"
+  name = 'licensing',         # How you named your package folder (MyLib)
+  packages = ['licensing'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Client library for Cryptolens licensing Web API.',   # Give a short description about your library
@@ -9,7 +9,7 @@ setup(
   author_email = 'support@cryptolens.io',      # Type in your E-Mail
   url = 'https://cryptolens.io',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['software licensing', 'licensing library', 'cryptolens'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pycrypto'
       ],
