31c042efdee7858d9e38dc41e2b243687d2fbe3c
Artem Los <arteml@kth.se>
2019-01-24 15:54:40 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
           'pycrypto'
       ],
   classifiers=[
-    'Development Status :: 5 - Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    #'Development Status :: 5 - Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
