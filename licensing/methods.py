ceeee0ca3e6e01c589a8ffa45f925c10200e7cdf
Thomas Gamauf <thomas.gamauf@gkcs.eu>
2019-10-04 13:07:07 +0200

    Fix error if API returns return code 1 as int (#12)

--- licensing/methods.py
+++ licensing/methods.py
@@ -81,7 +81,7 @@ class Key:
         
         jobj = json.loads(response)
 
-        if jobj == None or jobj["result"] == "1":
+        if jobj == None or str(jobj["result"]) == "1":
             if jobj != None:
                 return (None, jobj["message"])
             else:
