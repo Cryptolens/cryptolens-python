c15b40c83d2e1ecbcffc7a5f38e01db21b5d8731
Contact <74024690+CryptoContact@users.noreply.github.com>
2024-08-17 10:10:29 -0700

    Update setup.py
    spelling

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
